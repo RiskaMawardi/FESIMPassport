c157a6dda987bdb9d192756df6c696a638c19326
RiskaMawardi <riskamawardi439@gmail.com>
2023-05-29 16:19:56 +0700

    FE USer

--- resources/views/user/permohonan.blade.php
+++ resources/views/user/permohonan.blade.php
@@ -12,45 +12,54 @@
                       <div class="col-md-6">
                       <div class="form-group">
                       <label for="exampleInputUsername1">Nama Pemohon</label>
-                      <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Username">
+                      <input type="text" class="form-control" name="name" id="exampleInputUsername1" placeholder="Nama Lengkap">
                     </div>
                     <div class="form-group">
                       <label for="exampleInputEmail1">NIK</label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                      <input type="text" class="form-control" name="" id="exampleInputEmail1" placeholder="NIK">
                     </div>
                     <div class="form-group">
                       <label for="exampleInputEmail1">Alamat</label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                      <textarea type="text" class="form-control" name="" id="exampleInputEmail1" placeholder="Alamat"></textarea>
                     </div>
                     <div class="form-group">
                       <label for="exampleInputPassword1">Tgl Lahir</label>
-                      <input type="date" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                      <input type="date" class="form-control" name="" id="exampleInputPassword1" placeholder="Password">
                     </div>
                     <div class="form-group">
                       <label for="exampleInputConfirmPassword1">Tmp Lahir</label>
-                      <input type="text" class="form-control" id="exampleInputConfirmPassword1" placeholder="Password">
+                      <input type="text" class="form-control" name="" id="exampleInputConfirmPassword1" placeholder="Password">
                     </div>
                       </div>
                       <div class="col-md-6">
                       <div class="form-group">
                       <label for="exampleInputEmail1">Jk</label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                      <select class="form-control" name="" id="exampleSelectGender">
+                                <option>Laki-laki</option>
+                                <option>Perempuan</option>
+                            </select>
                     </div>
                     <div class="form-group">
                       <label for="exampleInputEmail1">Pekerjaan</label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                      <input type="text" class="form-control" name="" id="exampleInputEmail1" placeholder="Pekerjaan">
                     </div>
                     <div class="form-group">
                       <label for="exampleInputEmail1">No Tlp</label>
-                      <input type="number" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                      <input type="number" class="form-control" name="" id="exampleInputEmail1" placeholder="No Telepon">
                     </div>
                     <div class="form-group">
                       <label for="exampleInputEmail1">Kewarganegaraan</label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                      <select class="form-control" name="" id="exampleSelectGender">
+                            <option>WNI</option>
+                            <option>WNA</option>
+                        </select>
                     </div>
                     <div class="form-group">
                       <label for="exampleInputEmail1">Status Sipil</label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                      <select class="form-control" name="" id="exampleSelectGender">
+                                <option>Menikah</option>
+                                <option>Belum Menikah</option>
+                            </select>
                     </div>
                         </div>
                     </div>
